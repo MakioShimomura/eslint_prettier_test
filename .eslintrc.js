d660349601dc6d4f21a44b6f1c50ea08a645885e
mshimomura <mshimomura@fourmix.co.jp>
2021-10-15 16:03:35 +0900

    インデントを揃えた

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true, // require を使う時は追加
-  },
-  extends: [
-    // extends は後から追加した方のルールが優先される
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  rules: {
-    // プロジェクトに合わせたルールを設定
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    semi: ['error', 'always'],
-  },
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        semi: ['error', 'always'],
+    },
 };
